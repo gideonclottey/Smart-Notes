7d3e1f030c044679efca672512c74327a5fa9f86
gideonclottey <gideonclotteyint@gmail.com>
2023-03-06 19:01:31 +0000

    changing the form using django model forms

--- notes/forms.py
+++ notes/forms.py
@@ -5,9 +5,17 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title','text')
+        widgets ={
+            'title':forms.TextInput(attrs={'class':'form-control my-5'}),
+            'text': forms.Textarea(attrs={'class':'form-control mb-5'})
+        }
+        labels = {
+            'text':'Whats on your mind?',
+        }
 
     def clean_title(self):
         title = self.cleaned_data['title']
         if 'Django' not in title:
             raise ValidationError('we only accept notes about Django')
         return title
+    
